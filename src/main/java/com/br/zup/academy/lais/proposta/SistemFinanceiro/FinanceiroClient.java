d52758c39651920fd63ebd526738cf96f2a4f948
Lais Mukai <lais.mukai@zup.com.br>
2021-05-12 12:07:38 -0300

    arrumado caminho de dominios para API externas

--- src/main/java/com/br/zup/academy/lais/proposta/SistemFinanceiro/FinanceiroClient.java
+++ src/main/java/com/br/zup/academy/lais/proposta/SistemFinanceiro/FinanceiroClient.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "solicitacao", url = "http://localhost:9999")
+@FeignClient(value = "solicitacao", url = "${financeiro.dominio}")
 @Component
 public interface FinanceiroClient {
 
-    @RequestMapping(method = RequestMethod.POST, value = "/api/solicitacao", produces = "application/json")
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public AvaliacaoRespose avaliacao(AvaliacaoRequest request);
 }
--- src/main/java/com/br/zup/academy/lais/proposta/SistemaCartao/CartaoClient.java
+++ src/main/java/com/br/zup/academy/lais/proposta/SistemaCartao/CartaoClient.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "cartao", url = "http://localhost:8888")
+@FeignClient(value = "cartao", url = "${cartao.dominio}")
 @Component
 public interface CartaoClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/api/cartoes?idProposta={id}")
+    @RequestMapping(method = RequestMethod.GET)
     public CartaoRespose retornaNumeroCartao(@PathVariable("idProposta") String idProposta);
 
 }

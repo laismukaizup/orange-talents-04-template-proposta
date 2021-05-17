2c6799ce48d71e926532823bda5e169cb166c5ed
Lais Mukai <lais.mukai@zup.com.br>
2021-05-17 12:30:28 -0300

    arrumando erro de mapeamento de pastas

--- src/main/java/com/br/zup/academy/lais/proposta/SistemaCartao/AvisoRespose.java
+++ src/main/java/com/br/zup/academy/lais/proposta/SistemaCartao/AvisoRespose.java
@@ -1,9 +1,2 @@
-package com.br.zup.academy.lais.proposta.SistemaCartao;
-
-public class AvisoRespose {
-    public ResultadoAviso resultado;
-
-    public ResultadoAviso getResultado() {
-        return resultado;
-    }
+package com.br.zup.academy.lais.proposta.SistemaCartao;public class AvisoRespose {
 }
